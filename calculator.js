6e0ae1ae824754e9c238d0acf5e7a666552c559d
mariannaas <marianna.ask@gmail.com>
2018-08-06 22:38:29 +0200

    clear element added

--- calculator.js
+++ calculator.js
@@ -9,7 +9,9 @@ class Calculator {
         this.equalElement = document.getElementsByName('calculate')[0];
         this.displayInput = document.getElementById("display");
         this.digitValueElements = document.getElementsByClassName('digit');
+        this.clearElement = document.getElementById("clear");
         this.isOperatorClicked = false;
+        this.numberToCalculate = 0;
         this.initialize();
     }
 
@@ -20,21 +22,23 @@ class Calculator {
             return 0;
         }
     }
-    setDigitIntoDisplay(num){
-        if ((this.displayInput.value === "") && num === "0") {
+
+    setDigitIntoDisplay(num) {
+        if ((this.displayInput.value === '') && num === '0') {
             return;
         }
-        else if (this.isOperatorClicked === true) {
+        else if (this.isOperatorClicked) {
             this.displayInput.value = num;
             this.isOperatorClicked = false;
+            this.numberToCalculate = this.displayValue;
         }
         else {
             this.displayInput.value += num;
         }
     }
 
-    initialize() {
 
+    initialize() {
 
         this.addElement.addEventListener('click', (event) => {
             this.add();
@@ -52,42 +56,52 @@ class Calculator {
         this.divideElement.addEventListener('click', (event) => {
             this.divide();
         });
-        for(let i = 0; i < this.digitValueElements.length; i++) {
+        for (let i = 0; i < this.digitValueElements.length; i++) {
             this.digitValueElements[i].addEventListener('click', (event) => {
                 console.log("Clicked index: " + i);
                 this.setDigitIntoDisplay(this.digitValueElements[i].value);
-            })
+            });
         }
+        this.clearElement.addEventListener('click', (event) => {
+            this.clearAll();
+        });
 
     }
 
     add() {
-        this.result += this.displayValue;
+        this.result += this.numberToCalculate;
         this.clearResult();
     }
 
     subtract() {
-        this.result -= this.displayValue;
+        this.result -= this.numberToCalculate;
         this.clearResult();
     }
 
     multiply() {
-        this.result *= this.displayValue;
+        this.result *= this.numberToCalculate;
         this.clearResult();
     }
+
     divide() {
-        this.result /= this.displayValue;
+        this.result /= this.numberToCalculate;
         this.clearResult();
     }
+
     equation() {
         this.displayInput.value = this.result;
-     }
+    }
 
     clearResult() {
         this.isOperatorClicked = true;
-        this.displayInput.value = '';
+        this.displayInput.value = ''
     }
 
+    clearAll() {
+        this.clearResult();
+        this.result = 0;
+        this.numberToCalculate = 0;
+    }
 }
 
 let calculator = new Calculator();
